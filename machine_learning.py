6fbab7b283ee647b84cdfde7f5131eeb71f4b0f6
sumeet singh <singhsumeet2007@gmail.com>
2018-10-23 18:30:14 +0200

    Linear regression Scaler Transform

--- machine_learning.py
+++ machine_learning.py
@@ -1,16 +1,36 @@
+import matplotlib.pyplot as plt
 import numpy
 import pandas as pd
 from keras.layers import Dense
 from keras.models import Sequential
 from keras.wrappers.scikit_learn import KerasRegressor
-from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import KFold
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import train_test_split
-from sklearn.neural_network import MLPRegressor
+from sklearn.preprocessing import StandardScaler
 
 from data_dependency import data_corelation_spring_layout
 
+
+def linear_regression(x,y):
+    x_train, x_test = train_test_split(x, test_size=0.2,random_state=0)
+    y_train, y_test = train_test_split(y, test_size=0.2,random_state=0)
+
+    scalaer = StandardScaler().fit(x_train)
+    x_train = scalaer.transform(x_train)
+    x_test = scalaer.transform(x_test)
+
+    model = LinearRegression().fit(x_train,y_train)
+    #print(model.)
+    print(model.score(x_train,y_train))
+    y_pred = model.predict(x_test)
+    # y_train.values.reshape(-1,1)
+    # plt.scatter(x_train,y_train)
+    # plt.plot(x_train,y_pred)
+    # #x = linespace(10, 40, 5)
+    # #plt.plot(x,x,'-')
+    plt.show()
 
 def baseline_model():
     model = Sequential()
@@ -26,21 +46,31 @@ def linear_regression_using_data_depency_graph(file):
     G,df = data_corelation_spring_layout(file)
     col_names = list(df['var1'].unique())
     data_df = pd.read_csv(file)
+    normalized_df = (data_df - data_df.mean()) / data_df.std()
     for col_nm in col_names:
         temp_df = pd.DataFrame(df.loc[df['var1'] == col_nm])
         xval = temp_df['var2']
         #yval = temp_df['node'].values
-        x = data_df[xval]
-        y = data_df[col_nm]
-        print(x.shape,y.shape)
-        seed = 42
-        numpy.random.seed(seed)
-        # evaluate model with standardized dataset
-        estimator = KerasRegressor(build_fn=baseline_model, epochs=100, batch_size=10, verbose=0)
-        kfold = KFold(n_splits=10, random_state=seed)
-        results = cross_val_score(estimator, x, y, cv=kfold)
-        print("Results: %.2f (%.2f) MSE  %.2f accuracy" % (results.mean(), results.std(),results))
-        estimator.save('D:\Thesis\model\model'+col_nm+'.h5')
+        x = normalized_df[xval]
+        y = normalized_df[col_nm]
+        #sc = StandardScaler()
+        #x = sc.fit_transform(x_train)
+        linear_regression(x,y)
+
+        # seed = 42
+        # numpy.random.seed(seed)
+        # # evaluate model with standardized dataset
+        # estimator = KerasRegressor(build_fn=baseline_model, epochs=100, batch_size=10, verbose=0)
+        # kfold = KFold(n_splits=10, random_state=seed)
+        # results = cross_val_score(estimator, x, y, cv=kfold)
+        # print("Results: %.2f (%.2f) MSE " % (results.mean(), results.std()))
+        # # serialize model to JSON
+        # model_json = estimator.to_json()
+        # with open("D:\Thesis\model\model"+col_nm+".json", "w") as json_file:
+        #     json_file.write(model_json)
+        # # serialize weights to HDF5
+        # estimator.save_weights("model.h5")
+        # print("Saved model to disk")
 
 
 def linear_regression_using_data(file):
@@ -48,22 +78,30 @@ def linear_regression_using_data(file):
     column_name = list(df)
     model_list = []
     for col_name in column_name:
-        x_train, x_test = train_test_split(df.loc[:, df.columns != col_name], test_size=0.2)
-        y_train, y_test = train_test_split(df[col_name], test_size=0.2)
-        print(x_train.shape,x_test.shape)
-        print(y_train.shape,y_test.shape)
+        x = df.loc[:, df.columns != col_name]
         # sc = StandardScaler()
-        # x_train = sc.fit_transform(x_train)
-        # y_train = sc.transform(y_train)
-        model  = MLPRegressor(hidden_layer_sizes=(25,),  activation='tanh', solver='adam')
+        # x_scaler = sc.fit_transform(x)
+        y = df[col_name]
+        linear_regression(x,y)
 
-        model.fit(x_train,y_train)
-        y_pred = model.predict(x_test)
-        #print('Coefficients for '+col_name +'\n', model.coef_)
-        # The mean squared error
-        print("Mean squared error for "+col_name+": %.2f"% mean_squared_error(y_test, y_pred))
-        # Explained variance score: 1 is perfect prediction
-        print('Variance score for '+col_name+': %.2f' % r2_score(y_test, y_pred))
+        # x_train, x_test = train_test_split(x_scaler, test_size=0.2)
+        # y_train, y_test = train_test_split(df[col_name], test_size=0.2)
+        #
+        # # sc = StandardScaler()
+        # # x_train = sc.fit_transform(x_train)
+        # # y_train = sc.transform(y_train)
+        # model  = MLPRegressor(hidden_layer_sizes=(25,),  activation='tanh', solver='adam')
+        #
+        # model.fit(x_train,y_train)
+        # print(model.score(x_train,y_train))
+        #
+        # y_pred = model.predict(x_test)
+        # print(y_pred)
+        # #print('Coefficients for '+col_name +'\n', model.coef_)
+        # # The mean squared error
+        # print("Mean squared error for "+col_name+": %.2f"% mean_squared_error(y_test, y_pred))
+        # # Explained variance score: 1 is perfect prediction
+        # print('Variance score for '+col_name+': %.2f' % r2_score(y_test, y_pred))
 
 
 
@@ -98,4 +136,6 @@ def linear_regression_using_keras(file):
 
 
 
-linear_regression_using_data_depency_graph("D:\Thesis\Sampled Realtime Data from PF.csv")
+
+
+linear_regression_using_data("D:\Thesis\Sampled Realtime Data from PF.csv")
